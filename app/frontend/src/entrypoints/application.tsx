816e25eec26168d73c3db7543788c326c9acbbae
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-06-29 01:01:06 +0900

    refactor: eslintのwarningを修正する

--- app/frontend/src/entrypoints/application.tsx
+++ app/frontend/src/entrypoints/application.tsx
@@ -3,11 +3,11 @@ import App from '../App'
 
 // TasksController #index
 document.addEventListener('DOMContentLoaded', async () => {
-  const element = document.getElementById('tasks_index')
-  if (element!) {
+  const element = document.getElementById('tasks_index') || undefined
+  if (element != undefined) {
     const TasksIndex = (await import('../features/tasks/Index')).default
 
-    createRoot(element!).render(
+    createRoot(element).render(
       <App>
         <TasksIndex {...element?.dataset} />
       </App>,
